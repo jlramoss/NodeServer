cc85e3fd6df5b54b34a8613acae6594b376c21f6
jorge ramos <jlramoss@yahoo.com.mx>
2022-01-28 21:07:28 -0600

    initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/*
+1. First install dependencies and framework. 
+install nodemon by using  "npm install nodemon --save -dev"
+refreshes the console anytime you make changes to the file
+*/
+
+
+/* 2. next install babel which is a tool to convert ecmascript 2015 
+to the new version. so that old browsers can understand the new javascript.
+npm install @babel/core @babel/cli @babel/preset-env @babel/node @babel/runtime @babel/plugin-transform-runtime --save-dev
+*/
+
+/* 3. next install 
